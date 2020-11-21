48bf2f2e00a00d6c40721c69ee7ec8d7205b00c7
Nestor Orest Plysyuk <dayonel@hotmail.es>
2020-11-21 17:03:56 +0100

    Added project configuration and layers. (#2)

---
+++ BankingSystem.Data/BankingSystemDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankingSystem.Data
+{
+    public class BankingSystemDbContext : DbContext
+    {
+        public BankingSystemDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) { }
+    }
+}
